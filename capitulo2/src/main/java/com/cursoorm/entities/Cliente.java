64e3808f5a9ac1d2389aeca151f591c9f2a20fc9
pedrocpacheco <cpedropacheco@hotmail.com>
2023-06-22 14:10:07 -0300

    capitulo 2 - cliente entity

---
+++ capitulo2/src/main/java/com/cursoorm/entities/Cliente.java
@@ -0,0 +1,48 @@
+package com.cursoorm.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+
+    @Id
+    @SequenceGenerator(name = "cliente", sequenceName = "sq_tb_cliente", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "cliente")
+    @Column(name = "id_cliente")
+    private int id;
+
+    @Column(name = "nm_cliente")
+    private String nome;
+
+    public Cliente() {
+    }
+    
+    public Cliente(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
